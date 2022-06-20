c6e03fe81d5b29a32329ff1da569f7b4e1014351
AR123456 <coloradoz99@gmail.com>
2022-06-20 08:08:33 -0600

    nav bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,57 @@
 import React from "react";
 // import { Route } from "react-router-dom";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Button,
+  Offcanvas,
+  Form,
+  FormControl,
+} from "react-bootstrap";
 const Header = () => {
   return (
-    <div>
+    <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">My Pill Box </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#link">My Pill Box</Nav.Link>
+              <Nav.Link href="#link">Medications</Nav.Link>
+              <Nav.Link href="#link">SignUp</Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end">
+              {/* <Form className="d-flex">
+              show if on medications page 
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form> */}
+              <Navbar.Text>
+                Signed in as: <a href="#login">John Doe</a>
+              </Navbar.Text>
+              {/* <Button>
+              if not logged in 
+                {" "}
+                <Nav.Link href="#link">Sign In </Nav.Link>
+              </Button>
+              <Button>
+                {" "}
+                <Nav.Link href="#link">Sign Up </Nav.Link>
+              </Button> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </div>
+    </>
   );
 };
 
